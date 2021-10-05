fba4f0127d02637bdaaf23f5578fd0c64d6b2de6
Isaac Xu <playerxu@gmail.com>
2021-10-05 15:26:23 -0700

    demo shit

---
+++ newtest.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    x = "ECE-180_DA_DA"
+    if x == "EE_180_DA_DB":
+        print("You are living this year")
+    else:
+        #this is a comment
+        x = x + " - Best class evern"
+        print(x)
