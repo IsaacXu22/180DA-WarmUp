17040b61866153536f098ee3424fd7f6d881a1e1
Isaac Xu <playerxu@gmail.com>
2021-10-05 23:57:56 -0700

    Adds in videocapture

---
+++ videocapture.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2 as cv
+
+
+#What this does is capture a video in black and white
+cap = cv.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv.imshow('frame',gray)
+
+    #pretty sure this is the button to close the thing
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+#When everything is done, release the capture
+cap.release()
+cv.destroyAllWindows()
+
