a5c519a287c906b836f14d431e61018c991e8d65
Isaac Xu <playerxu@gmail.com>
2021-10-06 18:26:31 -0700

    testing the bounding box functions

--- boundingbox.py
+++ boundingbox.py
@@ -1,22 +1,18 @@
 import numpy as np
 import cv2 as cv
-
-img = cv.imread('star.jpg', 0)
-
-ret, thresh = cv.threshold(img, 127, 255, 0)
-
-contours, hierarchy = cv.findContours(thresh, 1, 2)
-
+import matplotlib as plt
+img = cv.imread('star.jpg',0)
+ret,thresh = cv.threshold(img,127,255,0)
+contours,hierarchy = cv.findContours(thresh, 1, 2)
 cnt = contours[0]
 M = cv.moments(cnt)
-print(M)
+print( M )
 
-cx = int(M['m10']/M['m00'])
-cy = int(M['m01']/M['m00'])
+#straight bounding rectangle
+x,y,w,h = cv.boundingRect(cnt)
 
-area = cv.contourArea(cnt)
-print('The area is: ' + str(area))
-
-perimeter = cv.arcLength(cnt, True)
-print('The perimeter is: ' + str(perimeter))
-
+while(True):
+    #cv.imshow('Contour', img)
+    cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
